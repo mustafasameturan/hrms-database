04dbc70c5f71af353300dfc7879a579147446a8a
Mustafa Samet Turan <mustafasameturan@hotmail.com>
2021-05-25 11:47:29 +0300

    initial commit

--- src/main/java/kodlamaio/hrms/core/verification/VerificationService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.core.verification;
-
-import kodlamaio.hrms.core.utilities.results.Result;
-
-public interface VerificationService {
-	Result sendLink(String email);
-	void sendCode(String email);
-	
-}
